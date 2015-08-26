86989fb2a4fb33379fa300418e57309c83546ce3
Adrian Domingo <adriandomingo@catalyst.net.nz>
2015-08-26 12:25:38 +1200

    add configuration settings for block

--- src/Plugin/Block/GreetingStatus.php
+++ src/Plugin/Block/GreetingStatus.php
@@ -3,6 +3,7 @@
 namespace Drupal\greeting\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Reports on greetability status
@@ -18,7 +19,37 @@ class GreetingStatus extends BlockBase {
   /**
    * {@inheritdoc}
    */
+  public function defaultConfiguration() {
+    return array('enabled' => 1);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form['enabled'] = array(
+      '#type' => 'checkbox',
+      '#title' => t('Greeting enabled'),
+      '#default_value' => $this->configuration['enabled'],
+    );
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['enabled'] =  $form_state->getValue('enabled');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function build() {
-    return array('#markup' => t('This is a basic block.'));
+    $message = $this->configuration['enabled']
+      ? $this->t('Now accepting greetings')
+      : $this->t('No greetings :-(');
+
+    return ['#markup' => $message];
   }
 }
